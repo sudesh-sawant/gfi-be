ac5b615f4f20dfc2c2f656bc187b445d36f81322
Sudesh Sawant <sudesh.sawant@gmail.com>
2025-04-19 16:55:56 +0530

    Initial commit for be-api. let's see how it goes.

---
+++ be-api/src/main/java/com/gfi/be/api/be_api/BeApiApplication.java
@@ -0,0 +1,13 @@
+package com.gfi.be.api.be_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApiApplication.class, args);
+	}
+
+}
---
+++ be-api/src/test/java/com/gfi/be/api/be_api/BeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfi.be.api.be_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
